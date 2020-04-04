62eb1310bd6a2a12b36cf9a5a9c7bb00da463533
Vasilii Fedorov <Vasilogo@gmail.com>
2020-04-04 21:08:33 +0200

    informace o fungovani funkce

--- vytvoreni_generace.py
+++ vytvoreni_generace.py
@@ -3,6 +3,12 @@ import random
 mnozstvi_jedincu = int(input("Zadejte mnozstvi potřebných jedincu: "))
 
 def vytvoreni_generace(sezn_cisel, mnozstvi_jedincu):
+    """
+    vytvari seznam obsahujici jednotlive seznamy s nahodne promichanymi cisly, predstavujici jedincy
+    :param sezn_cisel: seznam cisel od 1 do M(mnozstvi mest)
+    :param mnozstvi_jedincu: zadejme kolik jedincu budeme chtit v generace, ktera pak se bude menit ale mnozstvi jedincu ne.
+    :return:
+    """
     generace = []
     for i in range(mnozstvi_jedincu):
         generace.append(random.sample(sezn_cisel, len(sezn_cisel)))
