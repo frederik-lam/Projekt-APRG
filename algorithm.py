cb2c1d892a49126de5e57879767965362717ed2c
Vasilii Fedorov <Vasilogo@gmail.com>
2020-04-07 19:25:06 +0200

    hlavni algoritm

--- algorithm.py
+++ algorithm.py
@@ -1,12 +1,12 @@
 import ruzne_funkci as rf
 
 
-#[matice, M] = rf.nacteni_souboru(input("Bez uvazovok napiste nazev souboru: "))
+##[matice, M] = rf.nacteni_souboru(input("Bez uvazovok napiste nazev souboru: "))
 [matice, M] = rf.nacteni_souboru("data_vzdalenosti.csv")
 mnozstvi_jedincu = 4#int(input("Zadejte mnozstvi potřebných jedincu v generace: "))
 start_city = 1#int(input("Zadejte cislo zacatku cesty: "))
 
-generace = rf.create_generation(M, mnozstvi_jedincu,start_city) #[[4, 2, 3, 6, 5, 1], [4, 6, 2, 1, 3, 5], [4, 1, 2, 6, 5, 3], [4, 3, 1, 2, 6, 5]]
+generace = rf.create_generation(M, mnozstvi_jedincu,start_city) # vysl = [[4, 2, 3, 6, 5, 1], [4, 6, 2, 1, 3, 5], [4, 1, 2, 6, 5, 3], [4, 3, 1, 2, 6, 5]]
 kvality = rf.urceni_kvality(generace, matice)
 
 print(kvality)
