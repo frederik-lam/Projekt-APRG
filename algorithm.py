f6c5f852706a4d389ed318d3b1bc7498d446b8e8
Vasilii Fedorov <Vasilogo@gmail.com>
2020-04-08 19:32:11 +0200

    sdsd

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,11 @@
 import ruzne_funkci as rf
 
 
-[matice, M] = rf.file_read()
+[matice, poc_mest] = rf.file_read()
 mnozstvi_jedincu = 6#int(input("Zadejte mnozstvi potřebných jedincu v generace: "))
 start_city = 1#int(input("Zadejte cislo zacatku cesty: "))
 
-generace = rf.create_generation(M, mnozstvi_jedincu,start_city) # priklad vysledku = [[1, 2, 3, 6, 5, 4], [1, 6, 2, 4, 3, 5]]...
+generace = rf.create_generation(poc_mest, mnozstvi_jedincu,start_city) # priklad vysledku = [[1, 2, 3, 6, 5, 4], [1, 6, 2, 4, 3, 5]]...
 print(generace)
 kvality = rf.quality(generace, matice)
 print(kvality)
